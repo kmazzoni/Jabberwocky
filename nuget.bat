9962682a871a6b168ea4b0a5081234d69915dd89
Chris Smith <chris.smith@velir.com>
2016-02-23 17:19:46 -0500

    Revving the CodeAnalysis package version numbers

--- nuget.bat
+++ nuget.bat
@@ -6,8 +6,8 @@
 .nuget\NuGet.exe pack Jabberwocky.WebApi/Jabberwocky.WebApi.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
 .nuget\NuGet.exe pack Jabberwocky.Autofac/Jabberwocky.Autofac.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
 .nuget\NuGet.exe pack Jabberwocky.Autofac.Extras.MiniProfiler/Jabberwocky.Autofac.Extras.MiniProfiler.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
-.nuget\NuGet.exe pack Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.0 -OutputDirectory . -Prop Configuration=Release
-.nuget\NuGet.exe pack Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.0 -OutputDirectory . -Prop Configuration=Release
+.nuget\NuGet.exe pack Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis/Jabberwocky.Core.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
+.nuget\NuGet.exe pack Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis/Jabberwocky.Glass.CodeAnalysis.nuspec -NoPackageAnalysis -Version 1.0.2 -OutputDirectory . -Prop Configuration=Release
 
 del /F /Q Nuget
 move /Y *.nupkg Nuget
