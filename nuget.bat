7deb9f4cf0c6d73ad53cb3a4007c493779041e9c
Chris Smith <chris.smith@velir.com>
2016-02-23 15:54:16 -0500

    Adding missing nuspec for new WebApi project, and updated nuget.bat

--- nuget.bat
+++ nuget.bat
@@ -2,6 +2,7 @@
 .nuget\NuGet.exe pack Jabberwocky.Glass/Jabberwocky.Glass.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
 .nuget\NuGet.exe pack Jabberwocky.Glass.Autofac/Jabberwocky.Glass.Autofac.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
 .nuget\NuGet.exe pack Jabberwocky.Glass.Autofac.Mvc/Jabberwocky.Glass.Autofac.Mvc.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
+.nuget\NuGet.exe pack Jabberwocky.Glass.Autofac.WebApi/Jabberwocky.Glass.Autofac.WebApi.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
 .nuget\NuGet.exe pack Jabberwocky.WebApi/Jabberwocky.WebApi.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
 .nuget\NuGet.exe pack Jabberwocky.Autofac/Jabberwocky.Autofac.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
 .nuget\NuGet.exe pack Jabberwocky.Autofac.Extras.MiniProfiler/Jabberwocky.Autofac.Extras.MiniProfiler.csproj -IncludeReferencedProjects -symbols -Prop Configuration=Release
